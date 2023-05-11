38737b540da63e7b696f217d97240e709caecc42
Andrian Karsanashvili <andrian.karsanashvili@gmail.com>
2023-05-11 14:34:19 +0300

    :tada: optimize number of testimonials

--- src/pages/sections/Testimonials/Testimonials.tsx
+++ src/pages/sections/Testimonials/Testimonials.tsx
@@ -1,10 +1,37 @@
 import { testimonials } from "@/../public/testimonials";
 import { Button, Flex, Typography } from "@/components/common";
+import { breakpoints } from "@/constants";
+import { useWindowDimensions } from "@/hooks";
 import cn from "classnames";
 import { useTranslation } from "next-i18next";
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import s from "./Testimonials.module.scss";
 import { TestimonialsCard } from "./TestimonialsCard";
+
+const { biggerTablet, tablet } = breakpoints;
+
+function sliceAddTestimonials(width: number) {
+  // console.log("trigger", width);
+
+  let sliceTestimonials = 0;
+  let addTestimonials = 0;
+
+  if (width >= biggerTablet) {
+    sliceTestimonials = 6;
+    addTestimonials = 3;
+  } else if (width >= tablet) {
+    sliceTestimonials = 4;
+    addTestimonials = 2;
+  } else if (width < tablet && width > 0) {
+    sliceTestimonials = 2;
+    addTestimonials = 2;
+  }
+
+  return {
+    sliceTestimonials,
+    addTestimonials,
+  };
+}
 
 type TestimonialsTypes = {
   className: string;
@@ -12,15 +39,24 @@ type TestimonialsTypes = {
 
 export const Testimonials = memo(({ className }: TestimonialsTypes) => {
   const { t } = useTranslation(["initial", "common"]);
+  const { width } = useWindowDimensions();
+  const { sliceTestimonials, addTestimonials } = sliceAddTestimonials(width);
 
   const [testimonialsToRender, setTestimonialsToRender] = useState(
-    testimonials.slice(0, 6)
+    testimonials.slice(0, sliceTestimonials)
   );
+
+  useEffect(() => {
+    if (!testimonialsToRender.length) {
+      console.log("initialTistimonials", testimonialsToRender.length);
+      setTestimonialsToRender(testimonials.slice(0, sliceTestimonials));
+    }
+  }, [width]);
 
   function handleLoadMore() {
     setTestimonialsToRender((prev) => [
       ...prev,
-      ...testimonials.slice(prev.length, prev.length + 3),
+      ...testimonials.slice(prev.length, prev.length + addTestimonials),
     ]);
   }
 
