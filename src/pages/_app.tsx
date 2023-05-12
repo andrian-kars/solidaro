8619eff7403105a2937194faaef9d1019cd1bb9c
Andrian Karsanashvili <andrian.karsanashvili@gmail.com>
2023-05-12 18:34:40 +0300

    :tada: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { Layout } from "@/components/layouts/Layout";
 import "@/styles/index.scss";
+import { Analytics } from "@vercel/analytics/react";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import "./style.scss";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    <Analytics />
+  </>
 );
 
 // This default export is required
